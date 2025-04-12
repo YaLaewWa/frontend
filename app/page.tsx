58c56f9e8865efc7db22bd4c74b0af29243fed65
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-04-12 15:54:10 +0700

    fix: forget to redirect to .env in github action

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,7 @@ import { Socket } from "@/app/socket";
 export default function Page() {
   return (
     <div>
-      {process.env.BACKEND_URL ?
-      <Socket backendUrl={process.env.BACKEND_URL}/>
-      :
-      <div/>
-      }
+      <Socket backendUrl={process.env.BACKEND_URL ?? ""}/>
     </div>
   );
 }
