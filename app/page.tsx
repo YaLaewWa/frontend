6b71423607b9c47237417f11b39036f0ef207bf6
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-04-12 23:20:17 +0700

    test: vercel env

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Socket } from "@/app/socket";
 
 export default function Page() {
+  console.log(process.env.BACKEND_URL)
   return (
     <div>
       <Socket backendUrl={process.env.BACKEND_URL ?? ""}/>
