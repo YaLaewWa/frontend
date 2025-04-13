8d75b12a7b55674a48123ba5b76e8d2209067886
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-04-13 19:23:04 +0700

    refactor: project structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
-import { SocketHandler } from "@/components/socketHandler";
+import { SocketHandler } from "@/components/global/socketHandler";
 
 export default function Page() {
-
   return (
     <div>
-      {process.env.BACKEND_URL ?
-      <SocketHandler backendUrl={process.env.BACKEND_URL}/>
-      :
-      <div/>
-      }
+      {process.env.BACKEND_URL ? (
+        <SocketHandler backendUrl={process.env.BACKEND_URL} />
+      ) : (
+        <div />
+      )}
     </div>
   );
 }
