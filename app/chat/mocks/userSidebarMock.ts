a470e2878cb3dd60481eb71acbb81d56fa05ecd0
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-04-13 16:02:57 +0700

    chore: add mock data

---
+++ app/chat/mocks/userSidebarMock.ts
@@ -0,0 +1,27 @@
+export const unsortedUsersMock = [
+    {
+      username: "Friend1",
+      timestamp: new Date("2023-11-08T14:27:53Z"),
+      messageCount: 30
+    },
+    {
+      username: "Friend2",
+      timestamp: new Date("2024-04-19T07:12:30Z"),
+      messageCount: 30
+    },
+    {
+      username: "Friend3",
+      timestamp: new Date("2025-02-03T22:45:11Z"),
+      messageCount: 30
+    },
+    {
+      username: "Friend4",
+      timestamp: new Date("2022-09-25T05:33:19Z"),
+      messageCount: 30
+    },
+    {
+      username: "Friend5",
+      timestamp: new Date("2026-07-14T18:59:42Z"),
+      messageCount: 30
+    },
+  ];
