ded925f64caf5de9c5514effa3f9d79484b8af51
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-04-13 16:08:50 +0700

    refactor: change messageNumber to unread

--- app/chat/classes/UserClass.ts
+++ app/chat/classes/UserClass.ts
@@ -1,5 +1,5 @@
 export interface FriendBarInterface{
     username :string
     timestamp : Date
-    messageCount: number
+    unread: number
 }
--- app/chat/mocks/userSidebarMock.ts
+++ app/chat/mocks/userSidebarMock.ts
@@ -2,26 +2,26 @@ export const unsortedUsersMock = [
     {
       username: "Friend1",
       timestamp: new Date("2023-11-08T14:27:53Z"),
-      messageCount: 30
+      unread: 33
     },
     {
       username: "Friend2",
       timestamp: new Date("2024-04-19T07:12:30Z"),
-      messageCount: 30
+      unread: 47
     },
     {
       username: "Friend3",
       timestamp: new Date("2025-02-03T22:45:11Z"),
-      messageCount: 30
+      unread: 59
     },
     {
       username: "Friend4",
       timestamp: new Date("2022-09-25T05:33:19Z"),
-      messageCount: 30
+      unread: 26
     },
     {
       username: "Friend5",
       timestamp: new Date("2026-07-14T18:59:42Z"),
-      messageCount: 30
+      unread: 98
     },
   ];
