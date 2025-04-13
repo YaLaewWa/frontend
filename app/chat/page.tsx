0703a1fcef4a36955e233d2db38404df56d01642
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-04-13 20:53:58 +0700

    refator: move utility area into utility window directory

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,5 +1,5 @@
 import { FriendSideBar } from "@/app/chat/components/friendSideBar";
-import { UtilityArea } from "@/app/chat/components/utilityArea";
+import { UtilityArea } from "@/app/chat/components/utilityWindow/utilityArea";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
 export default async function Page({
