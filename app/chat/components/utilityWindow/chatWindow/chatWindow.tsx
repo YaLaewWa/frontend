a8ec3e84a6aca3955bdad8054dffddc2156ec72b
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-04-16 11:46:45 +0700

    feat: display mock message on screen

--- app/chat/components/utilityWindow/chatWindow/chatWindow.tsx
+++ app/chat/components/utilityWindow/chatWindow/chatWindow.tsx
@@ -1,7 +1,29 @@
+import { MessageBox } from "@/app/chat/components/utilityWindow/chatWindow/messageBox";
+import { chatMock } from "@/app/chat/mocks/chatMock";
+
 export function ChatWindow() {
+  const currentUser = "Friend0";
+  const unsortedChats = chatMock.chat;
+  const sortedChats: messageInterface[] = unsortedChats.sort((n1, n2) => {
+    if (n1.timestamp < n2.timestamp) {
+      return 1;
+    } else if (n1.timestamp > n2.timestamp) {
+      return -1;
+    }
+
+    return 0;
+  });
   return (
-    <div>
-      <p>This is chat</p>
+    <div className="w-full">
+      {sortedChats.map((message) => (
+        <MessageBox
+          key={currentUser + message.timestamp}
+          currentUser={currentUser}
+          sender={message.sender}
+          timestamp={message.timestamp}
+          message={message.message}
+        />
+      ))}
     </div>
   );
 }
