77bd6b0c9842d0baceed724845d751cb5ed842c2
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-04-16 18:55:29 +0700

    feat: add send time

--- app/chat/components/utilityWindow/chatWindow/messageBox.tsx
+++ app/chat/components/utilityWindow/chatWindow/messageBox.tsx
@@ -12,30 +12,38 @@ export function MessageBox({
 }: MessageBoxProp) {
   const userIsSender = sender === currentUser;
   // if (sender === currentUser) {
-    return (
-      <div className={`flex my-1 ${userIsSender ? "justify-end" : ""}`}>
-        <div className={`max-w-[300px] ${userIsSender ? "mr-2" : "ml-3"}`}>
-          <div className={`flex ${userIsSender ? "justify-end" : ""}`}>
-            <p className={`text-sm text-gray-500 ${userIsSender ? "pr-2" : "pl-3"}`}>{sender}</p>
-          </div>
-          <div className="flex">
-            <p className={`py-[10px] px-[15px] border rounded-2xl max-w-[300px] wrap-break-word ${userIsSender ? " bg-black text-white " : ""}`}>
-              {message}
-            </p>
-          </div>
+  return (
+    <div className={`flex my-1 ${userIsSender ? "justify-end" : ""}`}>
+      <div className={`max-w-[300px] ${userIsSender ? "mr-2" : "ml-3"}`}>
+        <div className={`flex ${userIsSender ? "justify-end" : ""}`}>
+          <p
+            className={`text-sm text-gray-500 ${
+              userIsSender ? "pr-2" : "pl-3"
+            }`}
+          >
+            {sender}
+          </p>
+        </div>
+        <div className="flex">
+          <p
+            className={`py-[10px] px-[15px] border rounded-2xl max-w-[300px] wrap-break-word ${
+              userIsSender ? " bg-black text-white " : ""
+            }`}
+          >
+            {message}
+          </p>
+        </div>
+        <div className={`flex ${userIsSender ? "justify-end" : ""}`}>
+          <p
+            className={`text-sm text-gray-500 ${
+              userIsSender ? "pr-2" : "pl-3"
+            }`}
+          >
+            {timestamp.toLocaleTimeString().slice(0, -6) +
+              timestamp.toLocaleTimeString().slice(-3)}
+          </p>
         </div>
       </div>
-    );
-  // } else {
-  //   return (
-  //     <div className="flex my-1">
-  //       <div className="max-w-[300px] ml-1">
-  //         <p className="text-sm">{sender}</p>
-  //         <p className="py-[10px] px-[15px] border rounded-2xl max-w-[300px] wrap-break-word">
-  //           {message}
-  //         </p>
-  //       </div>
-  //     </div>
-  //   );
-  // }
+    </div>
+  );
 }
