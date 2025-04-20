6b47c65d5b03c3b092db807669ac47814d483a57
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-04-20 18:45:01 +0700

    feat: add sonner toast into login page

--- app/login/components/loginForm.tsx
+++ app/login/components/loginForm.tsx
@@ -22,6 +22,7 @@ import {
 import Link from "next/link";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -49,7 +50,7 @@ export function LoginForm() {
       password: values.password,
     });
     if (res?.error) {
-      console.log(res.error);
+      toast.error(res.error);
     } else {
       router.push("/chat");
     }
