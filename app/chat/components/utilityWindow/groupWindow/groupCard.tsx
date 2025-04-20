c7f5060093c2c3ea88c1ad6ec121e320cc25e401
Naron <naron834@gmail.com>
2025-04-21 01:10:11 +0700

    feat: add join button

--- app/chat/components/utilityWindow/groupWindow/groupCard.tsx
+++ app/chat/components/utilityWindow/groupWindow/groupCard.tsx
@@ -1,23 +1,27 @@
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
 import React from 'react';
 
 export const GroupCard = ({ groupName, members }: GroupCardInterface) => {
   return (
-    <div className="border-2 hover:border-gray-700 rounded-xl w-full p-3 gap-2 flex flex-col">
-      <h1 className="text-xl">{groupName}</h1>
-      <div className="flex gap-1">
-        {members &&
-          members.length > 0 &&
-          members.map((d) => {
-            return (
-              <Avatar className="items-center w-[40px] h-[40px] border">
-                <AvatarFallback>
-                  {d.username.split(' ').map((e) => e.charAt(0))}
-                </AvatarFallback>
-              </Avatar>
-            );
-          })}
+    <div className="flex border-2 hover:border-gray-700 rounded-xl p-3 items-end">
+      <div className="w-full gap-2 flex flex-col">
+        <h1 className="text-xl">{groupName}</h1>
+        <div className="flex gap-1">
+          {members &&
+            members.length > 0 &&
+            members.map((d) => {
+              return (
+                <Avatar className="items-center w-[40px] h-[40px] border">
+                  <AvatarFallback>
+                    {d.username.split(' ').map((e) => e.charAt(0))}
+                  </AvatarFallback>
+                </Avatar>
+              );
+            })}
+        </div>
       </div>
+      <Button>Join</Button>
     </div>
   );
 };
