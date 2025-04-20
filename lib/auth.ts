9bb01e8adc2683294d4032864d84a18bf982c89b
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-04-20 19:04:25 +0700

    style: prettier fix

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,6 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 
-export function auth(){
-  return getServerSession(authOptions)
+export function auth() {
+  return getServerSession(authOptions);
 }
