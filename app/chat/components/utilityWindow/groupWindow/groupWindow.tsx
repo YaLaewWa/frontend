cafc990053786eed60487337614a45d6afe3a272
Naron <naron834@gmail.com>
2025-04-21 00:58:51 +0700

    feat: create group card

--- app/chat/components/utilityWindow/groupWindow/groupWindow.tsx
+++ app/chat/components/utilityWindow/groupWindow/groupWindow.tsx
@@ -1,7 +1,23 @@
+import { GroupCard } from "@/app/chat/components/utilityWindow/groupWindow/groupCard";
+import { groupMock } from "@/app/chat/mocks/groupMock";
+import { Divider } from "@/components/ui/divider";
+
 export function GroupWindow() {
+  const groups = groupMock.data
   return (
-    <div>
-      <p>This is search group</p>
+    <div className="p-10 w-full border-2 flex flex-col items-center gap-3">
+      <h1 className="text-4xl w-full">Group</h1>
+      <Divider className="w-full"/>
+      <div className="w-full flex flex-col gap-2">
+        {groups && groups.length > 0
+        ? groups.map((d) => {
+          return (
+            <GroupCard groupName={d.groupName} members={d.members} />
+          )
+        }) : (
+          <p></p>
+        )}
+      </div>
     </div>
   );
 }
---
+++ app/chat/types/Group.ts
@@ -0,0 +1,6 @@
+interface GroupCardInterface{
+    groupName: string
+    members: {
+        username: string
+    }[]
+}
