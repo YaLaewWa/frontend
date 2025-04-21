7b1d727780ae4d61001d90974546362e614a3760
Naron <naron834@gmail.com>
2025-04-21 14:28:06 +0700

    feat: add create group button

--- app/chat/components/utilityWindow/groupWindow/groupWindow.tsx
+++ app/chat/components/utilityWindow/groupWindow/groupWindow.tsx
@@ -1,13 +1,20 @@
 import { GroupCard } from '@/app/chat/components/utilityWindow/groupWindow/groupCard';
 import { groupMock } from '@/app/chat/mocks/groupMock';
+import { Button } from '@/components/ui/button';
 import { Divider } from '@/components/ui/divider';
+import Link from 'next/link';
 
 export function GroupWindow() {
   const groups = groupMock.data;
   return (
     <div className="p-10 w-full border-2 flex flex-col items-center gap-3">
-      <h1 className="text-4xl w-full">Group</h1>
-      <Divider className="w-full" />
+      <div className="flex justify-between w-full">
+        <h1 className="text-4xl w-full">Group</h1>
+        <Link href={'/chat?mode=CREATE'}>
+          <Button>Create Group</Button>
+        </Link>
+      </div>
+      <Divider className="w-full mb-3" />
       <div className="w-full flex flex-col gap-2">
         {groups && groups.length > 0 ? (
           groups.map((d) => {
