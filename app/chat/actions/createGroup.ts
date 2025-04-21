cd34bb8a4db75f64c44886e15312315970273b17
Naron <naron834@gmail.com>
2025-04-21 22:02:07 +0700

    feat: insert access token in action

--- app/chat/actions/createGroup.ts
+++ app/chat/actions/createGroup.ts
@@ -1,13 +1,15 @@
 'use server';
 
+import { auth } from '@/lib/auth';
+
 export const createGroup = async (groupName: string) => {
-  const access_token = 'access_token';
+  const session = await auth();
   try {
     const res = await fetch(`${process.env.BACKEND_URL}/group`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${access_token}`,
+        Authorization: `Bearer ${session?.accessToken}`,
       },
       body: JSON.stringify({ groupName: groupName }),
     });
