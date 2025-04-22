e082ed0e68bd66ad9d05ced7c57b23faa3326f34
Naron Chatjitakornkul <naron834@gmail.com>
2025-04-22 19:58:42 +0700

    fix: lint

--- app/components/utilityWindow/friendWindow/friendCard.tsx
+++ app/components/utilityWindow/friendWindow/friendCard.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
-import { useSession } from 'next-auth/react';
 
 export const FriendCard = ({
   username,
@@ -10,7 +9,6 @@ export const FriendCard = ({
   username: string;
   isYourself: boolean;
 }) => {
-  const { data } = useSession();
   return (
     <div className="flex border-2 hover:border-gray-700 rounded-xl p-3 items-center gap-3">
       <Avatar className="items-center w-[40px] h-[40px] border">
