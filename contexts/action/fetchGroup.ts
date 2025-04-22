c490841c104b35a3099375725d7daf0b62dae662
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-04-22 22:03:40 +0700

    feat: add fetchGroup

---
+++ contexts/action/fetchGroup.ts
@@ -0,0 +1,22 @@
+'use server';
+
+import { auth } from '@/lib/auth';
+
+export const fetchGroup = async (groupName: string) => {
+  const session = await auth();
+  const res = await fetch(`${process.env.BACKEND_URL}/chats/group?limit=-1&page=0`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${session?.accessToken}`,
+    },
+  });
+  const data = await res.json();
+  if (res.ok && data) {
+    return data;
+  } else if (!res.ok) {
+    return { message: data.error };
+  } else {
+    return { message: 'Something is wrong' };
+  }
+};
