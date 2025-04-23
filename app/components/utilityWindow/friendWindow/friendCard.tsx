d3e3c32eac8d818f7721886456d53e26922cf645
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-04-23 07:59:12 +0700

    feat: set active chat after press chat

--- app/components/utilityWindow/friendWindow/friendCard.tsx
+++ app/components/utilityWindow/friendWindow/friendCard.tsx
@@ -3,6 +3,7 @@ import { checkDM } from '@/app/actions/checkDM';
 import { createDM } from '@/app/actions/createDM';
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
+import { useWebSocketContext } from '@/contexts/WebsocketContext';
 import { useRouter } from 'next/navigation';
 
 export const FriendCard = ({
@@ -13,6 +14,7 @@ export const FriendCard = ({
   isYourself: boolean;
 }) => {
   const router = useRouter();
+  const { setActiveChat } = useWebSocketContext();
   return (
     <div className="flex border-2 hover:border-gray-700 rounded-xl p-3 items-center gap-3">
       <Avatar className="items-center w-[40px] h-[40px] border">
@@ -28,9 +30,11 @@ export const FriendCard = ({
           onClick={async () => {
             const res = await checkDM(username);
             if (res.status) {
+              setActiveChat(res.id);
               router.push(`/?id=${res.id}`);
             } else {
               const response = await createDM(username);
+              setActiveChat(response.id);
               router.push(`/?id=${response.id}`);
             }
           }}
